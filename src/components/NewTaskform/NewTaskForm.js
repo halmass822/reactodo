1c8e023d9509afbb1c0215b98be44a999c808de6
Hazem Al Massri <93576140+halmass822@users.noreply.github.com>
2024-02-08 20:28:40 +0000

    new task form css complete

--- src/components/NewTaskform/NewTaskForm.js
+++ src/components/NewTaskform/NewTaskForm.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import "./NewTaskForm.css";
 
 export default function NewTaskForm(props) {
 
@@ -13,14 +14,16 @@ export default function NewTaskForm(props) {
     };
 
     return <form className="newTaskForm" onSubmit={handleSubmit}>
-        <label className="newTaskName">Name: 
+        <h3>Create new task</h3>
+        <hr></hr>
+        <label className="newTaskName">New Task Name: 
             <input type="text" onChange={(e) => setNewTaskName(e.target.value)} 
             value={newTaskName}
             className="nameInput"
             required
             ></input> 
         </label>
-        <label className="newTaskDetails">Details: 
+        <label className="newTaskDetails">New Task Details: 
             <textarea type="text" onChange={(e) => setNewTaskDetails(e.target.value)} 
             value={newTaskDetails}
             className="nameInput"
